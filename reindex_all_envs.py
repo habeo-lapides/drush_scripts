c0360d683613a1ebd66ecfa212e26d68d4d0bb57
Eric Michalsen <eric.michalsen@kidney.org>
2024-05-01 14:01:56 -0400

    Update script to run Drush command on specific envs

--- reindex_all_envs.py
+++ reindex_all_envs.py
@@ -1,22 +1,8 @@
 import subprocess
-import json
-# Function to get all environments for a Pantheon site
-def get_all_environments(site):
-    # Run Terminus command to get environments JSON
-    terminus_command = f'terminus site:environments {site} --format=json'
-    process = subprocess.Popen(terminus_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    # Parse JSON output to extract environment names
-    if output:
-        environments = json.loads(output)
-        return [env['id'] for env in environments]
-    else:
-        print(f"Error: {error.decode()}")
-        return []
-# Function to run Terminus Drush command on each environment
+
 def run_drush_on_environments(site, command):
-    # Get all environments for the site
-    environments = get_all_environments(site)
+    # List of Pantheon environments
+    environments = ['dev', 'test']  # Add more environments if needed
     for env in environments:
         # Construct the Terminus Drush command
         terminus_command = f'terminus drush {site}.{env} -- {command}'
@@ -30,7 +16,7 @@ def run_drush_on_environments(site, command):
             print(f"Error for {site}.{env}:")
             print(error.decode())
         print()
-# Example usage
+
 site = 'd10-lapides'  # Replace with your Pantheon site name
-command = 'sqpi-i'  # Replace with the Drush command you want to run
+command = 'sapi-i'  # Replace with the Drush command you want to run
 run_drush_on_environments(site, command)
